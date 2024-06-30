81eb12941b1c708af6784f9ee16a6df1e336dc79
Ahmad  Syamil <ahmadfayruzsyamil33@gmail.com>
2024-06-30 08:08:05 +0700

    migrate

---
+++ go.mod
@@ -0,0 +1,62 @@
+module tkbai-be
+
+go 1.22.0
+
+require (
+	cloud.google.com/go/firestore v1.15.0
+	github.com/coreos/go-oidc v2.2.1+incompatible
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/joho/godotenv v1.5.1
+	github.com/labstack/echo-contrib v0.17.1
+	github.com/labstack/echo/v4 v4.12.0
+	github.com/mattn/go-sqlite3 v1.14.22
+	github.com/rs/zerolog v1.32.0
+	golang.org/x/oauth2 v0.19.0
+	google.golang.org/api v0.172.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
+)
+
+require (
+	cloud.google.com/go v0.112.1 // indirect
+	cloud.google.com/go/compute v1.24.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/longrunning v0.5.5 // indirect
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/s2a-go v0.1.7 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.12.3 // indirect
+	github.com/gorilla/context v1.1.2 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/sessions v1.2.2 // indirect
+	github.com/jmoiron/sqlx v1.4.0 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pquerna/cachecontrol v0.2.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.49.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.49.0 // indirect
+	go.opentelemetry.io/otel v1.24.0 // indirect
+	go.opentelemetry.io/otel/metric v1.24.0 // indirect
+	go.opentelemetry.io/otel/trace v1.24.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+	google.golang.org/genproto v0.0.0-20240227224415-6ceb2ff114de // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240311132316-a219d84964c2 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240318140521-94a12d6c2237 // indirect
+	google.golang.org/grpc v1.63.2 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
+)
---
+++ tkbai-main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"embed"
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
+	"html/template"
+	"log"
+	"net/http"
+	"tkbai-be/config"
+	"tkbai-be/databases"
+	"tkbai-be/handler"
+	"tkbai-be/models"
+	"tkbai-be/routes"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var (
+	//go:embed public
+	embedPublic embed.FS
+
+	pages = []string{
+		"public/views/*.html",
+	}
+)
+
+func main() {
+	a := new(config.Apps)
+
+	a.Tkbai = echo.New()
+
+	// recover
+	a.Tkbai.Use(middleware.Recover())
+
+	// set cors
+	a.Tkbai.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{config.WebHost, config.APIHost},
+		AllowMethods: []string{echo.GET, echo.HEAD, echo.PATCH, echo.PUT, echo.POST, echo.DELETE},
+	}))
+
+	sessionStore := sessions.NewCookieStore([]byte(config.AppSessionSecret))
+	sessionStore.Options = &sessions.Options{
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteStrictMode,
+	}
+
+	//Use session middleware
+	a.Tkbai.Use(session.Middleware(sessionStore))
+
+	//logging
+	initLoggingMiddleware(a)
+
+	//init handler
+	handler.InitErrHandler(a)
+
+	//add routes
+	routes.BuildRoutes(a)
+
+	initTemplate(a)
+
+	err := databases.ConnectTkbaiDatabase()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a.Tkbai.Logger.Fatal(a.Tkbai.Start(config.SERVERPort))
+}
+
+func initLoggingMiddleware(ein *config.Apps) {
+	logger := config.Log
+
+	ein.Tkbai.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogURI:       true,
+		LogURIPath:   true,
+		LogStatus:    true,
+		LogRemoteIP:  true,
+		LogHost:      true,
+		LogRequestID: true,
+		LogError:     true,
+		LogMethod:    true,
+		LogValuesFunc: func(c echo.Context, values middleware.RequestLoggerValues) error {
+			if values.Error != nil {
+				logger.Error().
+					Str("URI", values.URI).
+					Str("METHOD", values.Method).
+					Int("STATUS", values.Status).
+					Str("IP", values.RemoteIP).
+					Str("HOST", values.Host).
+					Str("RequestID", values.RequestID).
+					Stack().Err(values.Error).Msg("")
+			} else {
+				logger.Info().
+					Str("URI", values.URI).
+					Str("METHOD", values.Method).
+					Int("STATUS", values.Status).
+					Str("IP", values.RemoteIP).
+					Str("HOST", values.Host).
+					Str("RequestID", values.RequestID).
+					Msg("Request")
+			}
+			return nil
+		},
+	}))
+}
+
+// init template
+func initTemplate(srv *config.Apps) {
+	t := &models.Template{
+		Templates: template.Must(template.ParseFS(embedPublic, pages...)),
+	}
+
+	srv.Tkbai.Renderer = t
+	srv.Tkbai.Use(middleware.StaticWithConfig(middleware.StaticConfig{
+		Filesystem: http.FS(embedPublic),
+	}))
+}
