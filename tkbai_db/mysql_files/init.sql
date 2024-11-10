a3e51ac6842508b5cd83c4b3fb1870e8f6bbaf9e
syamil04 <fayruzsyamil@jatelindo.co.id>
2024-11-10 16:53:06 +0700

    update init.sql

--- tkbai_db/mysql_files/init.sql
+++ tkbai_db/mysql_files/init.sql
@@ -3,28 +3,26 @@ CREATE DATABASE IF NOT EXISTS tkbai;
 USE tkbai;
 
 DROP TABLE IF EXISTS tkbai_data;
-
 DROP TABLE IF EXISTS tkbai_user;
 
 CREATE TABLE
     IF NOT EXISTS tkbai_data (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-        test_id VARCHAR(100),
-        name VARCHAR(50),
-        student_number VARCHAR(50),
-        major VARCHAR(30),
-        date_of_test VARCHAR(30),
-        toefl_score VARCHAR(5),
-        insert_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
+                                 id INTEGER PRIMARY KEY AUTO_INCREMENT,
+                                 student_id VARCHAR(100),
+                                 name VARCHAR(50),
+                                 student_number VARCHAR(50),
+                                 major VARCHAR(30),
+                                 date_of_administration VARCHAR(30),
+                                 insert_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 CREATE TABLE
     IF NOT EXISTS tkbai_user (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-        email VARCHAR(25),
-        password VARCHAR(50),
-        insert_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
+                                 id INTEGER PRIMARY KEY AUTO_INCREMENT,
+                                 email VARCHAR(25),
+                                 password VARCHAR(50),
+                                 insert_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 INSERT INTO
     tkbai_user (email, password)
